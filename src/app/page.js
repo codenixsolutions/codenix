4c40c23c9de9baedc504676509cd18fa7031293e
Madhuka Kumarage <madhukawishwajith1@gmail.com>
2024-02-23 07:21:43 +0530

    Launching Soon v1.4

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,18 @@
 "use client";
 import React, {useEffect} from "react";
 import {Prompt} from "next/font/google";
-import ReactPlayer from "react-player";
 
 const prompt = Prompt({subsets: ["latin"], weight: ["400", "700", "500", "900"], variable: ["--font-prompt"]});
 
 export default function Home() {
-	useEffect(() => {
-		const ctx = new (window.AudioContext || window.webkitAudioContext)();
-
-		const osc = ctx.createOscillator();
-
-		osc.connect(ctx.destination);
-
-		osc.start(0);
-		osc.stop(2);
-
-		document.querySelector("#video").onload;
-	});
-
 	return (
-		<main className={`h-screen flex justify-center items-center`}>
-			<div className="h-screen overflow-hidden absolute p-0 m-0">
-				<video id="video" preload="auto" loop autoPlay muted>
+		<main className={`h-screen w-screen flex justify-center items-center `}>
+			<div className="absolute h-screen w-screen hidden lg:inline md:inline overflow-hidden top-0 left-0">
+				<video className={"rotate-90 lg:rotate-0"} preload="auto" loop autoPlay muted>
 					<source src="/media/videos/network.mp4" type="video/mp4" />
 				</video>
 			</div>
-			<div className={`${prompt.variable} font-prompt text-9xl animate-pulse`}>Launching Soon</div>
+			<div className={`${prompt.variable} font-prompt lg:text-9xl md:text-6xl sm:text-6xl animate-pulse`}>Launching Soon</div>
 		</main>
 	);
 }
